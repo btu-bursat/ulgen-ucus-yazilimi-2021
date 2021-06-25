e27691ddc5d694f54b436e19c147b356d8de657f
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-06-25 11:17:27 +0300

    dosyadan okuma için iyileştirmeler

--- telemetri/mpu9255.py
+++ telemetri/mpu9255.py
@@ -1,3 +1,5 @@
+# BTU Ulgen, 2021
+
 from FaBo9Axis_MPU9250 import MPU9250
 import time
 import board
@@ -8,22 +10,45 @@ i2c = board.I2C()
 bmp180 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 bmp180.sea_level_pressure = 1013.25
 
+# dosyadan okuma basarili olana kadar okumaya calis
+## !!! ancak burada buyuk bir sikinti var !!!
+## eger bir sensor hata verirse diger calisan butun sensorlerden
+## cevap alinamayacak 
+## buna bi ara bakilmasi lazim
 while True:
-    with open("/home/pi/telemetri_verileri/ivme_x", "w") as f:
-        f.write("%f" % mpu9250.readAccel()['x'])
-    with open("/home/pi/telemetri_verileri/ivme_y", "w") as f:
-        f.write("%f" % mpu9250.readAccel()['y'])
-    with open("/home/pi/telemetri_verileri/ivme_z", "w") as f:
-        f.write("%f" % mpu9250.readAccel()['z'])
-    with open("/home/pi/telemetri_verileri/pitch", "w") as f:
-        f.write("%f" % mpu9250.readGyro()['x'])
-    with open("/home/pi/telemetri_verileri/roll", "w") as f:
-        f.write("%f" % mpu9250.readGyro()['y'])
-    with open("/home/pi/telemetri_verileri/yaw", "w") as f:
-        f.write("%f" % mpu9250.readGyro()['z'])
-    with open("/home/pi/telemetri_verileri/sicaklik", "w") as f:
-        f.write("%f" % bmp180.temperature)
-    with open("/home/pi/telemetri_verileri/basinc", "w") as f:
-        f.write("%f" % bmp180.pressure)
-    with open("/home/pi/telemetri_verileri/yukseklik", "w") as f:
-        f.write("%f" % bmp180.altitude)
+	while True:
+		with open("/home/pi/telemetri_verileri/ivme_x", "w") as f:
+			f.write("%f" % mpu9250.readAccel()['x'])
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/ivme_y", "w") as f:
+			f.write("%f" % mpu9250.readAccel()['y'])
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/ivme_z", "w") as f:
+			f.write("%f" % mpu9250.readAccel()['z'])
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/pitch", "w") as f:
+			f.write("%f" % mpu9250.readGyro()['x'])
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/roll", "w") as f:
+			f.write("%f" % mpu9250.readGyro()['y'])
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/yaw", "w") as f:
+			f.write("%f" % mpu9250.readGyro()['z'])
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/sicaklik", "w") as f:
+			f.write("%f" % bmp180.temperature)
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/basinc", "w") as f:
+			f.write("%f" % bmp180.pressure)
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/yukseklik", "w") as f:
+			f.write("%f" % bmp180.altitude)
+			break
--- telemetri/telemetri.py
+++ telemetri/telemetri.py
@@ -1,59 +1,78 @@
+# BTU Ulgen, 2021
+
 import datetime
 import time
 
 while True:
 	start_time = time.time()
-
-	with open("/home/pi/telemetri_verileri/takim_no", "r") as f:
-		takim_no = f.read()
-
-	with open("/home/pi/telemetri_verileri/paket_numarasi", "r") as f:
-		paket_numarasi = f.read()
-
-	with open("/home/pi/telemetri_verileri/paket_numarasi", "w") as f:
-		f.write(str(int(paket_numarasi) + 1))
-
-	with open("/home/pi/telemetri_verileri/basinc", "r") as f:
-		basinc = f.read()
-
-	with open("/home/pi/telemetri_verileri/yukseklik", "r") as f:
-		yukseklik = f.read()
-
-	with open("/home/pi/telemetri_verileri/inis_hizi", "r") as f:
-		inis_hizi = f.read()
-
-	with open("/home/pi/telemetri_verileri/sicaklik", "r") as f:
-		sicaklik = f.read()
-
-	with open("/home/pi/telemetri_verileri/pil_gerilimi", "r") as f:
-		pil_gerilimi = f.read()
-
-	with open("/home/pi/telemetri_verileri/gps_latitude", "r") as f:
-		gps_latitude = f.read()
-
-	with open("/home/pi/telemetri_verileri/gps_longitude", "r") as f:
-		gps_longitude = f.read()
-
-	with open("/home/pi/telemetri_verileri/gps_altitude", "r") as f:
-		gps_altitude = f.read()
-
-	with open("/home/pi/telemetri_verileri/uydu_statusu", "r") as f:
-		uydu_statusu = f.read()
-
-	with open("/home/pi/telemetri_verileri/pitch", "r") as f:
-		pitch = f.read()
-
-	with open("/home/pi/telemetri_verileri/roll", "r") as f:
-		roll = f.read()
-
-	with open("/home/pi/telemetri_verileri/yaw", "r") as f:
-		yaw = f.read()
-
-	with open("/home/pi/telemetri_verileri/donus_sayisi", "r") as f:
-		donus_sayisi = f.read()
-
-	with open("/home/pi/telemetri_verileri/video_aktarim_bilgisi", "r") as f:
-		video_aktarim_bilgisi = f.read()
+	while True:
+		with open("/home/pi/telemetri_verileri/takim_no", "r") as f:
+			takim_no = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/paket_numarasi", "r") as f:
+			paket_numarasi = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/paket_numarasi", "w") as f:
+			f.write(str(int(paket_numarasi) + 1))
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/basinc", "r") as f:
+			basinc = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/yukseklik", "r") as f:
+			yukseklik = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/inis_hizi", "r") as f:
+			inis_hizi = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/sicaklik", "r") as f:
+			sicaklik = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/pil_gerilimi", "r") as f:
+			pil_gerilimi = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/gps_latitude", "r") as f:
+			gps_latitude = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/gps_longitude", "r") as f:
+			gps_longitude = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/gps_altitude", "r") as f:
+			gps_altitude = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/uydu_statusu", "r") as f:
+			uydu_statusu = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/pitch", "r") as f:
+			pitch = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/roll", "r") as f:
+			roll = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/yaw", "r") as f:
+			yaw = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/donus_sayisi", "r") as f:
+			donus_sayisi = f.read()
+			break
+	while True:
+		with open("/home/pi/telemetri_verileri/video_aktarim_bilgisi", "r") as f:
+			video_aktarim_bilgisi = f.read()
+			break
 
 	zaman = datetime.datetime.now()
 	gun_ay_yil = str(zaman.day) + "/" + str(zaman.month) + "/" + str(zaman.year)
@@ -67,9 +86,10 @@ while True:
 	telemetri += uydu_statusu + "," + pitch + "," + roll + "," + yaw + ","
 	telemetri += donus_sayisi + "," + video_aktarim_bilgisi
 	telemetri += "\n"
-
-	with open("/home/pi/telemetri_verileri/telemetri.txt", "at") as tf:
-		tf.write(telemetri)
+	while True:
+		with open("/home/pi/telemetri_verileri/telemetri.txt", "at") as tf:
+			tf.write(telemetri)
+			break
 
 	# 1 Hz de calismak icin dandik bir deneme; daha iyisini bulduysaniz bana bi
 	# selam cakin
