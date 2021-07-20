c8ae541370cd85cc9a77512c88aaf4a18835ea7b
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-07-20 11:34:49 +0300

    yanlış yerde oluşturulan kaynak dosyaları taşındı

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import mpu9250
 import bmp180
 import pil_yuzde
 import hiz
+import telemetri
 import wifi
 import komutlar
 import guc_yonetimi
@@ -19,6 +20,7 @@ def main():
 	thrd_bmp180 = Thread(target=bmp180.calistir)
 	thrd_pil_yuzde = Thread(target=pil_yuzde.calistir)
 	thrd_hiz = Thread(target=hiz.calistir)
+	thrd_telemetri = Thread(target=hiz.calistir)
 	thrd_wifi = Thread(target=wifi.calistir)
 	thrd_komutlar = Thread(target=komutlar.calistir)
 	thrd_guc_yonetimi = Thread(target=guc_yonetimi.calistir)
@@ -29,6 +31,7 @@ def main():
 	thrd_bmp180.start()
 	thrd_pil_yuzde.start()
 	thrd_hiz.start()
+	thrd_telemetri.start()
 	thrd_wifi.start()
 	thrd_komutlar.start()
 	thrd_guc_yonetimi.start()
