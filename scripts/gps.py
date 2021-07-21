7a834006bdab66ab0fb5ad4a05553df57ecb744a
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-07-21 22:28:31 +0300

    gps verisini almak için iskelet

--- scripts/gps.py
+++ scripts/gps.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 import telemetri_verileri as tv
+import serial
+from time import sleep
 
 def calistir():
-	pass
+	with serial.Serial('/dev/ttyS0', 9600) as ser:
+		while True:
+			#gps_verisi = ser.readline().strip().decode()
+			sleep(1)
