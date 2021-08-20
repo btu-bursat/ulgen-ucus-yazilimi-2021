e3f5e1789cdc332cefa01a5cd7c1784bc75e549f
Ceyhun Şen <ceyhunsen237@gmail.com>
2021-08-20 22:17:23 +0300

    otonom uçuş için iskelet

--- scripts/otonom_ucus.py
+++ scripts/otonom_ucus.py
@@ -2,12 +2,17 @@
 # Ulgen, 2021
 
 import telemetri_verileri as tv
-import motor
 from time import sleep
 
 def calistir():
 	return
 	while True:
 		if tv.uydu_statusu == "1":
+			if tv.ivme_y < 0 and tv.yukseklik >= 400:
+				tv.uydu_statusu = "2"
+		elif tv.uydu_statusu == "2":
+			if tv.ivme_y < 0 and tv.yukseklik <= 400:
+				tv.uydu_statusu = "3"
+		elif tv.uydu_statusu == "3":
 			pass
 		# devami da yazilacak
